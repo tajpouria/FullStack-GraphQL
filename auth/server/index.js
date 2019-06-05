a3ec78ff179b86fb8238132fbd27d5f5af4bce44
tajpouria <charliswager@gmail.com>
2019-06-05 17:34:49 +0430

    auth added

---
+++ auth/server/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const winston = require('winston');
+const bodyParser = require('body-parser');
+const expressGraphQL = require('express-graphql');
+const mongoose = require('mongoose');
+const config = require('config');
+
+const app = express();
+
+winston.add(winston.transports.File, { filename: 'logfile.log' });
+// DB setup
+const db = config.get('database.mongodb.uri');
+mongoose.connect(db, { useNewUrlParser: true }, err => {
+  if (err) throw new Error(err.message);
+
+  winston.info(`Successfully connected to ${db}`);
+});
+// middlewares
+app.use(bodyParser.json());
+app.use(
+  '/graphql',
+  expressGraphQL({
+    graphiql: true
+  })
+);
+//error handling
+process.on('uncaughtException', err => {
+  process.exit(1);
+  winston.error(err.message, err);
+});
+process.on('unhandledRejection', err => {
+  process.exit(1);
+  winston.error(err.message, err);
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => winston.info(`Listening on port ${port}...`));
