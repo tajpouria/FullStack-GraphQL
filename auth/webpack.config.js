8dd8049e51bce1e97728dd40c3c2c4f3c610f33d
tajpouria <charliswager@gmail.com>
2019-06-05 18:43:25 +0430

    half boilerplate added

---
+++ auth/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './client/src/index.js',
+  output: {
+    path: path.join(__dirname, '/client/dist'),
+    filename: 'index_bundle.js'
+  },
+  devServer: {
+    port: 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/public/index.html'
+    })
+  ]
+};
